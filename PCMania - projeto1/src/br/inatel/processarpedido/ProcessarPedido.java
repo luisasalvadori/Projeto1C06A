61ea46aeaca1977e5a918dde61dc7bbad73236e3
luisasalvadori <luisa.salvadori@gec.inatel.br>
2026-04-05 10:54:26 -0300

    Delete PCMania - projeto1 directory

--- PCMania - projeto1/src/br/inatel/processarpedido/ProcessarPedido.java
+++
@@ -1,9 +0,0 @@
-package br.inatel.processarpedido;
-
-import br.inatel.computador.Computador;
-
-public class ProcessarPedido {
-    public static void processar(Computador[] comps){
-        System.out.println("Pedido enviado...");
-    }
-}
